70573bc4273f53787edd58a76e31c7f6cd957315
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2022-09-03 19:42:40 +0300

    js1

---
+++ js1/app.js
@@ -0,0 +1,54 @@
+// this code show the app is running
+// console.log("app.js is running");
+// let ad = prompt("Adınızı girin");
+// console.log("Adınız: " + ad)
+// let sonuc = confirm('Emin misiniz?');
+// document.write('<h2>Full-Stack</h2>')
+// console.log("adınız : " + ad + "dır.")
+// console.log(`adınız : ${ad} dır.`)
+
+var myNumber = 5;
+console.log(myNumber1)
+
+
+
+
+
+var myNumber1 = 'merhaba';
+console.log(myNumber1)
+
+let var1 = 55;
+console.log(var1)
+
+var1 = 99;
+console.log(var1);
+
+{
+    let localVar1 = 'local variable';
+    var myName = 'ahmet';
+    console.log(localVar1);
+}
+// console.log(localVar1)
+console.log(myName);
+
+// const konusu
+
+const student = {
+    firstName : 'Can',
+    number :'F1234'
+}
+
+
+const otherStudent = student;
+console.log(otherStudent)
+
+otherStudent.number = 'F6789'
+
+console.log(student);
+
+/*otherStudent ={
+    firstName: 'Sinan',
+    number: 'F2365'
+};
+
+console.log(otherStudent) */
