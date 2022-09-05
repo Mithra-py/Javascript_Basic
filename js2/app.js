d2a159762f679a9c4beec9f9c73bcf2c7ca4fbd8
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2022-09-05 18:06:48 +0300

    js2

---
+++ js2/app.js
@@ -0,0 +1,94 @@
+// let a;
+// console.log(a);
+// console.log(b);
+// console.log('hello')
+
+
+// console.log(a);
+// var a = 3;
+// var aa = 2;
+// console.log(a);
+
+// console.log(b);
+// let b = 1;
+// console.log(b);
+
+// let c = '2';
+// let d = 2;
+
+// console.log(typeof c)
+// console.log(typeof d)
+
+// let e = +prompt('Bir şey yazınız');
+// console.log(typeof e);
+
+// let f = null;
+// console.log(typeof f);
+
+// let g = 3.14;
+// let h = 1_000_000;
+
+// console.log(typeof g)
+// console.log(typeof h)
+
+// let i = 999_999_999_999_999;
+// console.log(i)
+// let j = 999_999_999_999_868_123;
+// console.log(j)
+
+// console.log(015+025);
+// console.log(018+025);
+
+// console.log(0.1 + 0.2);
+
+// let l = 0.1 + 0.2
+// console.log(l)
+// console.log(l.toFixed(2));
+
+// let m = 'Hello World';
+// let n = "Merhaba Dunya";
+// let p = `Merhaba JS`
+
+// console.log(m)
+// console.log(n)
+// console.log(p)
+
+// let userName = 'Ahmet';
+// let pp = `Merhaba ${userName}`
+// console.log(pp)
+
+// const myCar = {
+//     make : 'ford',
+//     model : 'mustang',
+//     year : 1965,
+//     color : "black"
+// }
+// console.log(myCar)
+
+// myCar.color = 'red';
+// myCar.sunRoof = true;
+
+// myCar.age = function(current){
+//     console.log(current - this.year)
+// }
+
+// console.log(myCar);
+
+// myCar.age(2022);
+
+// delete myCar.sunRoof;
+
+// console.log(myCar)
+
+
+
+
+
+
+
+
+
+
+
+
+
