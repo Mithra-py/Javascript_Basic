75e79236b6a34bd09f127e35251b10c6c44c4a7c
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2022-09-06 19:11:04 +0300

    js3

---
+++ js3/app.js
@@ -0,0 +1,77 @@
+// if statement example
+// check if the is positive
+
+/*const n1 = +prompt('Enter a number: ');
+console.log(n1, typeof n1);
+if(n1 > 0) {
+    console.log(`${n1} is positive`);
+}else if (n1 < 0) {
+    console.log(`${n1} is negative`);
+}else {
+    console.log(`The number is zero`);
+}*/
+
+
+/*const bsize = prompt("Enter your body size: " );
+
+if(bsize < 32){
+    console.log(`Wrong Size`);
+}else if (bsize < 35){
+    console.log(`Your size(US) is XS`);
+}else if (bsize < 38){
+    console.log(`Your size(US) is S`);
+}else if (bsize < 41){
+    console.log(`Your size(US) is M`);
+}else if (bsize < 44){
+    console.log(`Your size(US) is L`);
+}else if (bsize < 47){
+    console.log(`Your size(US) is XL`);
+}else if (bsize < 50){
+    console.log(`Your size(US) is XXL`);
+}else {
+    console.log(`Wrong Size`)
+}*/
+
+
+/*const n1 = prompt("Enter your first number: ");
+const n2 = prompt("Enter your second number: ");
+const n3 = prompt("Enter your third number: ");
+
+if(n1 > n2 && n1 > n3){
+    console.log(`${n1} is the greatest among ${n2} and ${n3}`);
+}else if(n2 > n1 && n2 > n3){
+    console.log(`${n2} is the greatest among ${n1} and ${n3}`);
+}else if(n3 > n1 && n3 > n2){
+    console.log(`${n3} is the greatest among ${n1} and ${n2}`);
+}*/
+
+
+/*let grade = prompt(`Enter your score: `)
+let result = grade >= 50 ? 'pass' : 'fail';
+console.log(`You ${result} the exam.`);*/
+
+
+/*const n1 = 0;
+const result = n1 >= 0 ? (n1 == 0 ? `zero` : `${n1} is positive`) : 'negative';
+console.log(result)*/
+
+
+/*let text;
+let fruit = prompt('Type your favorite fruit');
+// banana, lime, limon, orange, apple, default
+switch (fruit.toLocaleLowerCase()){
+    case 'banana':
+        text = "I Like Banana!🍌"
+    break;
+    case 'lime':
+    case "lemon":
+    case "orange":
+        text = `I am not a fan of ${fruit}`;
+    break
+    case "apple":
+        text = 'I Like apple'
+    break
+    default:
+        text = `I don't know that fruit`;
+}
+console.log(text)*/
