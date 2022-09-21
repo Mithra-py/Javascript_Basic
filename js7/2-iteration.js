db8b868c0b348aa93c0ee03a59b8d4c94240aae8
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2022-09-21 17:53:28 +0300

    foreach and map

---
+++ js7/2-iteration.js
@@ -0,0 +1,76 @@
+//*                        FOREACH METHOD   
+console.log("********** ITERATION ************");
+
+// Dizideki her bir fiyatı konsola bastırınız.
+const fiyatlar = [100, 250, 50, 89];
+
+fiyatlar.forEach(yazdir)
+
+function yazdir(value){
+console.log(value);
+}
+
+console.log("*******************************")
+
+//! Arrow function kullanılarak çözüm.
+fiyatlar.forEach((value) => console.log(value));
+
+
+// Fiyatlar dizisindekilerin toplamını konsola yazdırınız.
+
+let toplam = 0
+fiyatlar.forEach((fiyat) => (toplam += fiyat))
+console.log("Toplam:", toplam);
+
+
+let toplam1 = 0
+
+// Array.forEach((value, index, array) => )
+fiyatlar.forEach((değer, indis, dizi) => {
+    toplam1 += değer
+    console.log(`${indis}.iterasyon toplamı: ${toplam1}`);
+    dizi[indis] = değer + 10;
+});
+console.log(fiyatlar)
+
+
+//                      MAP METHOD
+
+const isimler = [
+    "Ahmet",
+    "Mehmet",
+    "ismet",
+    "SAFFET",
+    "nur",
+    "CAN",
+    "Cem"
+];
+
+console.log(isimler);
+console.log(isimler.map((isim) => isim.toLocaleUpperCase()));
+
+const küçükİsimler = isimler.map((isim) => isim.toLowerCase());
+console.log(küçükİsimler)
+
+
+
+const büyükİsimler = isimler.map((isim, i, dizi) =>{
+    dizi[i] = isim.toLocaleLowerCase();
+    return isim.toLocaleUpperCase();
+});
+console.log(büyükİsimler);
+
+
+
+isimler
+.map((x) => x.toLocaleUpperCase())
+.forEach((name) => console.log(name));
+
+
+console.log("*****************************")
+
+
+isimler
+.filter((x) => x === "ahmet")
+.map((x) => x.toLocaleUpperCase())
+.forEach((x) => console.log(x));
